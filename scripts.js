dd4aedbada618356b573f9ab31fc11f1381da01e
Hoa Dinh <hoadinh.rdms@gmail.com>
2021-05-09 02:31:29 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//callback function
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function isEvenNumber(number) {
+  return number % 2 == 0;
+}
+
+const evenNumbers = numbers.filter(isEvenNumber);
+console.log(evenNumbers);
